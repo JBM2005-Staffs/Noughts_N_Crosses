231a56792b5651d4cfc46c2da9016317d915c2f2
JBM2005-Staffs <m018517o@student.staffs.ac.uk>
2024-11-26 23:25:03 +0000

    Final Final

--- Noughts_N_Crosses/Noughts_N_Crosses/Noughts_N_Crosses.cpp
+++ Noughts_N_Crosses/Noughts_N_Crosses/Noughts_N_Crosses.cpp
@@ -10,11 +10,11 @@ struct Player
 };
 
 char board[3][3] = { {'1', '2', '3'},
-                     {'4', '5', '6'},
+                     {'4', '5', '6'},           //Credit: geeksforgeeks for the starting point
                      {'7', '8', '9'} };
  
-char symbol = 'X'; // Current turn: 'X' or 'O'
-int moves = 0;     // Count the number of moves made
+char symbol = 'X'; 
+int moves = 0;     // Used to judge a drawer or not 
 bool draw = false;
 
 void printGameBoard() {
@@ -22,14 +22,13 @@ void printGameBoard() {
     cout << "  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << " \n";
     cout << "_____|_____|_____\n";
     cout << "    |     |     \n";
-    cout << "  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << " \n";       //Geeks for geeks had a different way to this, this concept I picked up from https://www.simplilearn.com/tutorials/cpp-tutorial/game-in-cpp
+    cout << "  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << " \n";       //Geeks for geeks had a different way to this, this concept I picked up from (https://www.simplilearn.com/tutorials/cpp-tutorial/game-in-cpp)
     cout << "_____|_____|_____\n";                                                                  // Lots of the methods I've found in my time researching how to make this,
     cout << "    |     |     \n";                                                                   // are very similar, this grid being quite popular which is why I'm also using it
     cout << "  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << " \n";
     cout << "    |     |     \n\n";
 }
 
-// Convert user input (1-9) into board coordinates
 bool inputConversion(int choice, int& row, int& col) 
 {
     if (choice < 1 || choice > 9) {
@@ -40,18 +39,16 @@ bool inputConversion(int choice, int& row, int& col)
     return true;
 }
 
-// Check if the selected cell is empty
+// Check cell is free
 bool isMoveValid(int row, int col) 
 {
     return board[row][col] != 'X' && board[row][col] != 'O';
 }
 
-// Update the board with the current player's symbol
 void updateBoard(int row, int col) 
 {
     board[row][col] = symbol;
 }
-
 
 void nextTurn() 
 {
@@ -60,7 +57,6 @@ void nextTurn()
 
 bool checkWin() 
 {
-
     for (int i = 0; i < 3; i++) 
     {
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) 
@@ -78,7 +74,7 @@ bool checkWin()
         }
     }
     
-    //Horizontals too
+    //Horizontals 
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) 
     {
         return true;
@@ -109,7 +105,7 @@ int main()
         << "      \\  /   |         |         " << endl
         << " C     \\/    |         |         " << endl
         << "       /\\    |         |         " << endl
-        << "      /  \\   |         |         \n\n" << endl;
+        << "      /  \\   |         |         \n" << endl;
     cout << " _   _                   _     _          ___     \n"
         << "| \\ | | ___  _   _  __ _| |__ | |_ ___   ( _ )         \n"
         << "|  \\| |/ _ \\| | | |/ _` | '_ \\| __/ __|  / _ \\/     \n"
@@ -122,51 +118,86 @@ int main()
         << "           Jacob McGrath 2024 \n\n\n";
 
     cout << "Hello Players, what would you like to be called?\n";
-    cout << "Player 1, enter your name\n";
+    cout << "Player 1, please enter your name\n";
     cout << "> ";
     getline(cin, player1.name);
+
+    cout << "\nPlayer 2, please enter your name\n";
+    cout << "> ";
+    getline(cin, player2.name);
 
     player1.isX = true;
     player2.isO = true;
 
-    cout << player1.name << " will play as 'X'.\n";
-    cout << player2.name << " will play as 'O'.\n";
+    cout << "\n" << player1.name << " will play as 'X'.\n";
+    cout << "\n" << player2.name << " will play as 'O'.\n";
 
-    // Game Loop
-    while (moves < 9) 
-    {
-        printGameBoard();
-        cout << ((symbol == 'X') ? player1.name : player2.name) << "'s turn (" << symbol << ").\n";
-        cout << "{Please choose a number (1-9): ";
+    while (moves < 9) { // Max 9 moves in a 3x3 Tic-Tac-Toe game
+        printGameBoard(); // Display the current state of the board
+        cout << "It's " << ((symbol == 'X') ? player1.name : player2.name) << "'s turn (" << symbol << ").\n";
 
-        int choice;
-        cin >> choice;
+        while (true) { // Input validation loop
+            cout << "\nEnter a value between 1 & 9: ";
+            int choice;
+            cin >> choice;
 
-        int row, col;
-        if (!inputConversion(choice, row, col) || !isMoveValid(row, col)) 
-        {
-            cout << "\nInvalid move. Try again.\n";
-            continue; 
+            // Ensure input is a valid number and within range 1-9
+            if (cin.fail() || choice < 1 || choice > 9) {
+                cin.clear();
+                cin.ignore(1); // Ignore invalid input
+                cout << "\n[!]ERROR: Invalid Input[!]\nThat position is full or outside accepted bounds. Try again.\n";
+                continue; // Retry input
+            }
+
+            int row, col;
+            if (!inputConversion(choice, row, col) || !isMoveValid(row, col)) 
+            {
+                cout << "\nInvalid input. Please enter a number between 1 and 9.\n";
+                continue; // Retry input
+            }
+
+            // Valid move
+            updateBoard(row, col);
+            moves++;
+            break; // Exit input validation loop
         }
 
-        // Make the move
-        updateBoard(row, col);
-        moves++;
-
-        // Check for a winner
+        // Check if the current player has won
         if (checkWin()) 
         {
-            printGameBoard();
-            cout << ((symbol == 'X') ? player1.name : player2.name) << " WON!\n";
-            cout << "Better luck next time " << ((symbol == 'X') ? player2.name : player1.name) << "!\n";   // Reverse them to get the inverse output. 
-            return 0; // End the game
+            cout << ((symbol == 'X') ? player1.name : player2.name) << " Wins! Congratulations!\n\n";
+            cout << "Better luck next time!, " << ((symbol == 'X') ? player2.name : player1.name) << "\n";
+
+            cout << " _   _                   _     _          ___     \n"
+                << "| \\ | | ___  _   _  __ _| |__ | |_ ___   ( _ )         \n"
+                << "|  \\| |/ _ \\| | | |/ _` | '_ \\| __/ __|  / _ \\/     \n"
+                << "| |\\  | (_) | |_| | (_| | | | | |_\\__ \\ | (_>  <     \n"
+                << "|_|_\\_|\\___/ \\__,_|\\__, |_| |_|\__|___/  \\___/\\/  \n"
+                << " / ___|_ __ ___  __|___/  ___  ___                      \n"
+                << "| |   | '__/ _ \\/ __/ __|/ _ \\/ __|                   \n"
+                << "| |___| | | (_) \\__ \\__ \\  __/\\__ \\                \n"
+                << " \\____|_|  \\___/|___/___/\\___||___/                  \n\n"
+                << "           Jacob McGrath 2024 \n\n\n";
+
+            return 0;
         }
 
-        // Switch turns
+        // Alternate turns
         nextTurn();
     }
 
     printGameBoard();
-    cout << "Game over! It's a draw.\n";
+    cout << "\n\nGame over! It's a draw.\n";
+    cout << " _   _                   _     _          ___     \n"
+        << "| \\ | | ___  _   _  __ _| |__ | |_ ___   ( _ )         \n"
+        << "|  \\| |/ _ \\| | | |/ _` | '_ \\| __/ __|  / _ \\/     \n"
+        << "| |\\  | (_) | |_| | (_| | | | | |_\\__ \\ | (_>  <     \n"
+        << "|_|_\\_|\\___/ \\__,_|\\__, |_| |_|\__|___/  \\___/\\/  \n"
+        << " / ___|_ __ ___  __|___/  ___  ___                      \n"
+        << "| |   | '__/ _ \\/ __/ __|/ _ \\/ __|                   \n"
+        << "| |___| | | (_) \\__ \\__ \\  __/\\__ \\                \n"
+        << " \\____|_|  \\___/|___/___/\\___||___/                  \n\n"
+        << "           Jacob McGrath 2024 \n\n\n";
+    
     return 0;
 }
